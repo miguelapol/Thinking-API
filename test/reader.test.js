6851c02524f5b1c2d771c01459a9202f4bafcef1
miguelapol <apolonio1995@gmail.com>
2022-04-30 14:43:56 -0500

    prueba

--- test/reader.test.js
+++ test/reader.test.js
@@ -1,9 +1,10 @@
 const Reader = require("./../app/utils/Reader");
 const path = require('path');
+const students = Reader.ReadJsonFile(path.resolve(__dirname,"../test/data/studentest.json"));
 // const file = fs.readFileSync(path.resolve(__dirname, '../'+ruta), 'utf8');
 describe("Pruebas de unidad", function() {
     test("Read of students Reader", function() {
-        const students = Reader.ReadJsonFile(path.resolve(__dirname,"../test/data/studentest.json"));
+       
         expect(students).not.toBeUndefined();
         
     });
